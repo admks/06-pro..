4f7597bed4283e47322f3a0ac28866fe31ce5ea1
mirza <alexbrost7@gmail.com>
2022-02-10 22:09:50 +0300

    06 day pro...

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import  "./App.css"
+
+export default function App() {
+  return (
+    <div className='parent'>
+      <h1 className='title'>Hello World</h1>
+      <h2 className='child'>How are you?</h2>
+      <p id='parap'>I m fine! </p>
+
+
+    </div>
+    
+  )
+}
